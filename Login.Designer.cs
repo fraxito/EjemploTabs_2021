3b726a1fe90178a414a99acae12fbf634875cfbe
Jorge Cisneros <CETYSDAM@DESKTOP-HMUGL0C>
2021-05-11 11:35:55 +0200

    Prueba de Merge (fusión de ramas)

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -33,6 +33,8 @@
             this.usuario = new System.Windows.Forms.TextBox();
             this.pass = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // botonLogin
@@ -84,12 +86,32 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Contraseña";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 291);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(261, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(81, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 33);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Contraseña";
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(327, 498);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pass);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.usuario);
@@ -109,5 +131,7 @@
         private System.Windows.Forms.TextBox usuario;
         private System.Windows.Forms.TextBox pass;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
